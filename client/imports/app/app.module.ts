0f63d6928a49b462863b3f9a78d8b14c0040715d
Matthew Yamout <Matthew Yamout>
2017-03-25 13:55:37 -0700

    Finished profile component template

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {SettingsPageComponent} from './settings/settings-page.component';
+import {ProfilePageComponent} from './profile/profile-page.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,7 @@ import {SettingsPageComponent} from './settings/settings-page.component';
   ],
   declarations: [
     AppComponent,
-    SettingsPageComponent
+    ProfilePageComponent
   ],
   bootstrap: [
     AppComponent
---
+++ client/imports/app/profile/profile-page.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import template from './profile-page.component.html';
+
+@Component({
+  selector: 'profile-page',
+  template
+})
+export class ProfilePageComponent {
+
+  user: Object;
+  constructor() {
+    this.user = {
+      'name': 'John Smith',
+      'email': 'john.smith@gmail.com',
+      'redid': '12345678'
+    };
+
+  }
+  /*
+    Runs method before rendering the component. We should use this method to load data from the database
+    to our component objects before rendering.
+  */
+  ngOnInit() {
+    //load users info from the local mongo database
+  }
+
+  manageClasses() {
+    alert('this function will route to the manage classes page');
+  }
+
+  userSettings() {
+    alert('this function will route to the user settings page');
+  }
+
+}
