8c7b8728943c4f91a5f629761ad74092aef1e78d
ole-lar <olechr@hotmail.com>
2017-03-25 17:36:30 -0700

    Working sub-component for checkin page

---
+++ client/imports/app/checkin/professor/checkin-professor.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import template from './checkin-professor.component.html';
+
+@Component({
+  selector: 'professor-checkin',
+  template
+})
+export class ProfessorPageComponent {}
