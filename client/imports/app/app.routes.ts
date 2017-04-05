9b4632f4bc09198afb7add0eb9724fc2dd300696
Matthew Yamout <Matthew Yamout>
2017-04-05 08:38:26 -0700

    Added profile-component to the routes

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/router';
 
+import { ProfilePageComponent } from './profile/profile-page.component';
 import { ClassesPageComponent } from './classes/classes-page.component';
 import { LoginPageComponent } from './login/login-page.component';
 import { CheckinPageComponent } from './checkin/checkin-page.component';
@@ -8,6 +9,7 @@ import { ManageClasses } from './manage/manage-classes.component';
 
 export const routes: Route[] = [
   { path: '', component: LoginPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
   { path: 'classes', component: ClassesPageComponent },
   { path: 'checkin', component: CheckinPageComponent },
   { path: 'manage', component: ManageClasses },
